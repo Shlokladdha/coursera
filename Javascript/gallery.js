335abaafef10e91545b5bebf618c13c25344a18d
Shlokladdha <64690358+Shlokladdha@users.noreply.github.com>
2020-05-31 13:58:26 +0530

    Update gallery.js

--- Javascript/gallery.js
+++ Javascript/gallery.js
@@ -1,9 +1,9 @@
 function upDate(previewPic){
-	  document.getElementById("image").style.background=previewPic.src;
+	  document.getElementById("image").style.backgroundImage="url('previewPic.src')";
     document.getElementById("image").innerHTML=.previewPic.alt;
 }
 
 function unDo(){
-		document.getElementById("image").style.background=#8e68ff;
+		document.getElementById("image").style.backgroundImage="none";
 		document.getElementById("image").innerHTML="Hover over an image below to display here.";
 }
