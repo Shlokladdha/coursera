88dad2b3cfabc74146dcf54b50eafcbf6b31727e
Shlokladdha <64690358+Shlokladdha@users.noreply.github.com>
2020-05-31 11:12:09 +0530

    Update gallery.js

--- Javascript/gallery.js
+++ Javascript/gallery.js
@@ -10,10 +10,9 @@ function upDate(previewPic){
     */
 	  document.getElementById("image").style.background=previewPic.src;
     document.getElementById("image").innerHTML=.previewPic.alt;
-  
-	}
+}
 
-	function unDo(){
+function unDo(){
      /* In this function you should 
     1) Update the url for the background image of the div with the id = "image" 
     back to the orginal-image.  You can use the css code to see what that original URL was
@@ -23,4 +22,4 @@ function upDate(previewPic){
     */
 		document.getElementById("image").style.background=#8e68ff;
 		document.getElementById("image").innerHTML="Hover over an image below to display here.";
-	}
+}
